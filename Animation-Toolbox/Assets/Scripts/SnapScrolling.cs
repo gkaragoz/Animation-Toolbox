c1721020690eb2aa03b38b93206aa65fd559a01c
gkaragoz <annotto3@gmail.com>
2018-06-02 21:36:51 +0300

    Make SnapScrolling.cs script to dynamic creation.

--- Animation-Toolbox/Assets/Scripts/SnapScrolling.cs
+++ Animation-Toolbox/Assets/Scripts/SnapScrolling.cs
@@ -5,10 +5,7 @@ using UnityEngine.UI;
 
 public class SnapScrolling : MonoBehaviour {
 
-    [Range(1, 50)]
     [Header("Controllers")]
-    public int panelCount;
-
     [Range(0, 500)]
     public int panelOffset;
 
@@ -25,9 +22,9 @@ public class SnapScrolling : MonoBehaviour {
     public GameObject panelPrefab;
     public ScrollRect scrollRect;
 
-    private GameObject[] _panelInstances;
-    private Vector2[] _panelPositions;
-    private Vector2[] _panelScales;
+    private List<GameObject> _panelInstances = new List<GameObject>();
+    private List<Vector2> _panelPositions = new List<Vector2>();
+    private List<Vector2> _panelScales = new List<Vector2>();
 
     private RectTransform _contentRect;
     private Vector2 _contentVector;
@@ -35,32 +32,38 @@ public class SnapScrolling : MonoBehaviour {
     private int _selectedPanelID;
     private bool _isScrolling;
 
-    private void Start() {
+    private void Awake() {
         _contentRect = GetComponent<RectTransform>();
+    }
 
-        _panelInstances = new GameObject[panelCount];
-        _panelPositions = new Vector2[panelCount];
-        _panelScales = new Vector2[panelCount];
+    public GameObject CreateAPanel() {
+        GameObject newPanel = Instantiate(panelPrefab, transform, false);
 
-        for (int ii = 0; ii < panelCount; ii++) {
-            _panelInstances[ii] = Instantiate(panelPrefab, transform, false);
+        Vector2 newPanelPosition = GetNewPanelPosition(newPanel);
+        newPanel.transform.localPosition = newPanelPosition;
 
-            if (ii == 0) continue;
+        _panelScales.Add(Vector2.one);
+        _panelPositions.Add(-newPanelPosition);
+        _panelInstances.Add(newPanel);
 
-            _panelInstances[ii].transform.localPosition = new Vector2(
-                _panelInstances[ii-1].transform.localPosition.x + panelPrefab.GetComponent<RectTransform>().sizeDelta.x + (panelOffset * 3f), 
-                _panelInstances[ii].transform.localPosition.y);
+        return newPanel;
+    }
 
-            _panelPositions[ii] = -_panelInstances[ii].transform.localPosition;
-        }
+    private Vector2 GetNewPanelPosition(GameObject newPanel) {
+        if (_panelInstances.Count == 0) return Vector2.zero;
+
+        int lastPanelIndex = _panelInstances.Count;
+
+        return new Vector2(_panelInstances[lastPanelIndex - 1].transform.localPosition.x + panelPrefab.GetComponent<RectTransform>().sizeDelta.x + (panelOffset * 3f),
+            newPanel.transform.localPosition.y);
     }
 
     private void FixedUpdate() {
-        if (_contentRect.anchoredPosition.x <= _panelPositions[0].x && !_isScrolling || _contentRect.anchoredPosition.x <= _panelPositions[_panelPositions.Length-1].x && !_isScrolling)
+        if (_contentRect.anchoredPosition.x <= _panelPositions[0].x && !_isScrolling || _contentRect.anchoredPosition.x <= _panelPositions[_panelPositions.Count-1].x && !_isScrolling)
             scrollRect.inertia = false;
 
         float nearestPos = float.MaxValue;
-        for (int ii = 0; ii < panelCount; ii++) {
+        for (int ii = 0; ii < _panelInstances.Count; ii++) {
             float distance = Mathf.Abs(_contentRect.anchoredPosition.x - _panelPositions[ii].x);
 
             if (distance < nearestPos) {
@@ -69,8 +72,8 @@ public class SnapScrolling : MonoBehaviour {
             }
 
             float scale = Mathf.Clamp(1 / (distance / panelOffset) * scaleOffset, 0.5f, 1f);
-            _panelScales[ii].x = Mathf.SmoothStep(_panelInstances[ii].transform.localScale.x, scale + 0.3f, scaleSpeed * Time.fixedDeltaTime);
-            _panelScales[ii].y = Mathf.SmoothStep(_panelInstances[ii].transform.localScale.y, scale + 0.3f, scaleSpeed * Time.fixedDeltaTime);
+            _panelScales[ii] = new Vector2(Mathf.SmoothStep(_panelInstances[ii].transform.localScale.x, scale + 0.3f, scaleSpeed * Time.fixedDeltaTime),
+                Mathf.SmoothStep(_panelInstances[ii].transform.localScale.y, scale + 0.3f, scaleSpeed * Time.fixedDeltaTime));
             _panelInstances[ii].transform.localScale = _panelScales[ii];
         }
 
