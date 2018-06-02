c46bd9e362c02bc1c94e5b4313a6e25a6be6b455
gkaragoz <annotto3@gmail.com>
2018-06-02 21:06:49 +0300

    Create AssetsLoader.cs script.

---
+++ Animation-Toolbox/Assets/Scripts/AssetsLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetsLoader : MonoBehaviour {
+
+    [System.Serializable]
+    public class AssetPackage {
+        public string folderName;
+        public Object[] assets;
+    }
+
+    [Header("Initialization")]
+    public AssetPackage[] assetPackages;
+
+	void Awake () {
+        LoadAssets();
+    }
+
+    void LoadAssets() {
+        for (int ii = 0; ii < assetPackages.Length; ii++) {
+            string folderName = assetPackages[ii].folderName;
+            assetPackages[ii].assets = Resources.LoadAll(folderName);
+        }
+    }
+
+    public Object[] GetLoadedAsset(string folderName) {
+        foreach (AssetPackage package in assetPackages) {
+            if (package.folderName == folderName) {
+                return package.assets;
+            }
+        }
+        return null;
+    }
+	
+}
