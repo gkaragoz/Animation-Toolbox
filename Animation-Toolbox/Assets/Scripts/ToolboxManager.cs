01d0e3fa115cc1d07fad4502d2c64ad8292c0577
gkaragoz <annotto3@gmail.com>
2018-06-02 21:37:02 +0300

    Create ToolboxManager.cs script.

---
+++ Animation-Toolbox/Assets/Scripts/ToolboxManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolboxManager : MonoBehaviour {
+
+    private SnapScrolling _snapScrolling;
+    private AssetsLoader _assetsLoader;
+
+    private void Awake() {
+        _snapScrolling = FindObjectOfType<SnapScrolling>();
+        _assetsLoader = FindObjectOfType<AssetsLoader>();
+    }
+
+    private void Start() {
+        for (int ii = 0; ii < 10; ii++) {
+            _snapScrolling.CreateAPanel();
+        }
+    }
+}
